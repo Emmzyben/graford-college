50b063e349cf7ded69a63409c0aced6201fb920f
Emmanuel <emmco96@gmail.com>
2023-11-26 17:16:33 +0100

    new

---
+++ javascript.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+    const subMenu = document.querySelector('.sub-menu1');
+    subMenu.style.display = (subMenu.style.display === 'none' || subMenu.style.display === '') ? 'block' : 'none';
+  }
