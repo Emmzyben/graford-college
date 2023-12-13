2ac52ce50d85e098cec37b9b4d0043276a509bc2
unknown <emmco96@gmail.com>
2023-12-13 15:07:42 -0800

    new

--- javascript.js
+++ javascript.js
@@ -61,4 +61,93 @@ function toggleDropdown() {
         updateImage();
         
 
-       
+    var open = false;
+        function open1(){
+            open = true; 
+            terminate();
+            terminate2();
+                document.getElementById('div1').style.display='block'; 
+                document.getElementById('link1').style.backgroundColor='white';
+                document.getElementById('link1').style.color='#0e0e88';
+            }
+            function open2(){
+                open = true; 
+                terminate();
+                terminate2();
+                    document.getElementById('div2').style.display='block'; 
+                    document.getElementById('link2').style.backgroundColor='white';
+                    document.getElementById('link2').style.color='#0e0e88';
+                }
+                function open3(){
+                    open = true; 
+                    terminate();
+                    terminate2();
+                        document.getElementById('div3').style.display='block'; 
+                        document.getElementById('link3').style.backgroundColor='white';
+                        document.getElementById('link3').style.color='#0e0e88';
+                    }    function open4(){
+                        terminate();
+                        terminate2();
+                        open = true; 
+                            document.getElementById('div4').style.display='block'; 
+                            document.getElementById('link4').style.backgroundColor='white';
+                            document.getElementById('link4').style.color='#0e0e88';
+                        }    function open5(){
+                            terminate();
+                            terminate2();
+                            open = true; 
+                                document.getElementById('div5').style.display='block'; 
+                                document.getElementById('link5').style.backgroundColor='white';
+                                document.getElementById('link5').style.color='#0e0e88';
+                            }    function open6(){
+                                terminate();
+                                terminate2();
+                                open = true; 
+                                    document.getElementById('div6').style.display='block'; 
+                                    document.getElementById('link6').style.backgroundColor='white'; 
+                                    document.getElementById('link6').style.color='#0e0e88';
+                                }
+         
+            function terminate(){
+                document.getElementById('div1').style.display='none';
+                document.getElementById('div2').style.display='none'; 
+                document.getElementById('div3').style.display='none'; 
+                document.getElementById('div4').style.display='none'; 
+                document.getElementById('div5').style.display='none'; 
+                document.getElementById('div6').style.display='none';
+            }
+            function terminate2(){
+                document.getElementById('link1').style.backgroundColor='#0e0e88'; 
+               document.getElementById('link1').style.color='white';
+
+               document.getElementById('link2').style.backgroundColor='#0e0e88'; 
+               document.getElementById('link2').style.color='white';
+
+               document.getElementById('link3').style.backgroundColor='#0e0e88'; 
+               document.getElementById('link3').style.color='white';
+
+               document.getElementById('link4').style.backgroundColor='#0e0e88'; 
+               document.getElementById('link4').style.color='white';
+
+               document.getElementById('link5').style.backgroundColor='#0e0e88'; 
+               document.getElementById('link5').style.color='white';
+
+               document.getElementById('link6').style.backgroundColor='#0e0e88'; 
+               document.getElementById('link6').style.color='white';
+            }
+
+
+            var eligible = false;
+            function drop(){
+               
+
+                if(eligible === false){
+                    document.getElementById('open').style.height='auto';
+                    document.getElementById('direct').innerHTML=`<i class="fa fa-toggle-down"></i>`;
+                    eligible=true; 
+                }else{
+                    document.getElementById('open').style.height='0px';
+                    document.getElementById('direct').innerHTML=`<i class="fa fa-caret-square-o-right"></i>`;
+                    eligible=false;
+                }
+            }
